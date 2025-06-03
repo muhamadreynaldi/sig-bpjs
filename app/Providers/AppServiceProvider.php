e251eddf9826304e7a5a78e9d51aed301939e938
muhamadreynaldi <m.reynaldi2003@gmail.com>
2025-06-04 04:51:49 +0700

    Update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator; // Tambahkan ini
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Paginator::useBootstrapFive(); // Tambahkan baris ini
+        // Jika Anda juga ingin menggunakan Bootstrap 4 di beberapa tempat (jarang terjadi)
+        // Paginator::useBootstrapFour();
+        // Atau secara umum
+        // Paginator::defaultView('view-name');
+        // Paginator::defaultSimpleView('view-name');
     }
 }
